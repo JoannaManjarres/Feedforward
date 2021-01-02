5eec2267867128c11603b74d3723ddfd8dc901a6
Jorge Caicedo <jrgcaicedo@gmail.com>
2021-01-02 02:13:35 -0300

    refactor: usando snake_case

--- final/code/preProcessing/pre_processing_baseline_output.py
+++ final/code/preProcessing/pre_processing_baseline_output.py
@@ -9,75 +9,74 @@ Created on Sun Jul 12 20:39:28 2020
 import numpy as np
 
 
-def beamsLogScale(y,thresholdBelowMax):
-        y_shape = y.shape
-        
-        for i in range(0,y_shape[0]):            
-            thisOutputs = y[i,:]
-            logOut = 20*np.log10(thisOutputs + 1e-30)
-            minValue = np.amax(logOut) - thresholdBelowMax
-            zeroedValueIndices = logOut < minValue
-            thisOutputs[zeroedValueIndices]=0
-            thisOutputs = thisOutputs / sum(thisOutputs)
-            y[i,:] = thisOutputs
-        
-        return y
+def beams_log_scale(y, threshold_below_max):
+    y_shape = y.shape
 
-def getBeamOutput(output_file):
-    
-    thresholdBelowMax = 1
-    
+    for i in range(0, y_shape[0]):
+        this_outputs = y[i, :]
+        log_out = 20 * np.log10(this_outputs + 1e-30)
+        min_value = np.amax(log_out) - threshold_below_max
+        zeroed_value_indices = log_out < min_value
+        this_outputs[zeroed_value_indices] = 0
+        this_outputs = this_outputs / sum(this_outputs)
+        y[i, :] = this_outputs
+
+    return y
+
+
+def get_beam_output(output_file):
+    threshold_below_max = 1
+
     print("Reading dataset...", output_file)
     output_cache_file = np.load(output_file)
-    yMatrix = output_cache_file['output_classification']
-    print("Forma yMatrix",yMatrix.shape)
-    yMatrix = np.abs(yMatrix)
-    yMatrix /= np.max(yMatrix)
-    yMatrixShape = yMatrix.shape
-    num_classes = yMatrix.shape[1] * yMatrix.shape[2]
-    
-    y = yMatrix.reshape(yMatrix.shape[0],num_classes)
-    y = beamsLogScale(y,thresholdBelowMax)
+    y_matrix = output_cache_file['output_classification']
+    print("Forma yMatrix", y_matrix.shape)
+    y_matrix = np.abs(y_matrix)
+    y_matrix /= np.max(y_matrix)
+    num_classes = y_matrix.shape[1] * y_matrix.shape[2]
 
-    return y,num_classes
+    y = y_matrix.reshape(y_matrix.shape[0], num_classes)
+    y = beams_log_scale(y, threshold_below_max)
 
-def generate_groups_beams(y,k):
-    labels=[]
-    
+    return y, num_classes
+
+
+def generate_groups_beams(y, k):
+    labels = []
+
     for i in range(y.shape[0]):
-        valor = round(np.argmax(y[i,:], axis=0)/k)
+        valor = round(np.argmax(y[i, :], axis=0) / k)
         labels.append(valor)
-    
-    
+
     return np.asarray(labels)
 
-########################## MAIN #####################################################
+
+# -------------------------- MAIN ------------------------------
 
 def process_and_save_output_beams(k):
-    if (k==0):
+    if k == 0:
         print("k should be greater than zero")
     else:
         baseline_path = "../../data/processed/output_beam/baseline/"
-        
-        #train
-        output_train_file = baseline_path+'beams_output_train.npz'
-        y_train,num_classes = getBeamOutput(output_train_file)
-        
-        output_validation_file = baseline_path+'beams_output_validation.npz'
-        y_validation, _ = getBeamOutput(output_validation_file)
-        
-        savePath = "../../data/processed/output_beam/"
-        debugPath = "../../data/processed/output_beam/debug/"
-        
-        #train
-        label_train = generate_groups_beams(y_train,k)
+
+        # train
+        output_train_file = baseline_path + 'beams_output_train.npz'
+        y_train, num_classes = get_beam_output(output_train_file)
+
+        output_validation_file = baseline_path + 'beams_output_validation.npz'
+        y_validation, _ = get_beam_output(output_validation_file)
+
+        save_path = "../../data/processed/output_beam/"
+        debug_path = "../../data/processed/output_beam/debug/"
+
+        # train
+        label_train = generate_groups_beams(y_train, k)
         debug_train = label_train[0:20]
-        np.savez(savePath+'beams_output_train'+'.npz',output_training=label_train)
-        np.savez(debugPath+'beams_output_train'+'.npz',output_training=debug_train)
+        np.savez(save_path + 'beams_output_train' + '.npz', output_training=label_train)
+        np.savez(debug_path + 'beams_output_train' + '.npz', output_training=debug_train)
 
-        #test
-        label_validation = generate_groups_beams(y_validation,k)
+        # test
+        label_validation = generate_groups_beams(y_validation, k)
         debug_validation = label_validation[0:2]
-        np.savez(savePath+'beams_output_validation'+'.npz',output_test=label_validation)
-        np.savez(debugPath+'beams_output_validation'+'.npz',output_test=debug_validation)
-
+        np.savez(save_path + 'beams_output_validation' + '.npz', output_test=label_validation)
+        np.savez(debug_path + 'beams_output_validation' + '.npz', output_test=debug_validation)
